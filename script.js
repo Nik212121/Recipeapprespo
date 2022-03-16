ffdc76f2c9b40479e50fe321d3e02fdfc030e2a6
Nik212121 <53296564+Nik212121@users.noreply.github.com>
2022-03-16 16:17:05 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//query selcetor
+
+const recipeForm = document.querySelector('#recipe-form');
+const recipeContainer = document.querySelector('#recipe-container');
+
+let listItems = [];
+
+//functions
+function handleFormSubmit(e) {
+    e.preventDefault();
+    const name = recipeForm.querySelector('#name').value;
+    const method = recipeForm.querySelector('#method').value;
+    const roast = recipeForm.querySelector('#roast').value;
+    const grind = recipeForm.querySelector('#grind').value;
+    const ratio = recipeForm.querySelector('#ratio').value;
+    const note = recipeForm.querySelector('#note').value;
+    const newRecipe = {
+        name,
+        method,
+        roast,
+        grind,
+        ratio,
+        note,
+        git
+        id: Date.now(),
+    }
+    console.log(newRecipe);
+
+}
+//envent listerns
+recipeForm.addEventListener('submit', handleFormSubmit);
